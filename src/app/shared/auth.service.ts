7a58a9ef4a5cff360565060995949087289298d3
KevElliot <r.kevinelliot@gmail.com>
2021-07-03 14:36:21 +0300

    init historique pari

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -9,10 +9,10 @@ import { Login } from '../model/login.model';
 export class AuthService {
 
     constructor(private http: HttpClient) { }
-    
+
     // uri = "http://localhost:8010/pariBack";
     uri = "https://parilocalnode.herokuapp.com/pariBack";
-    
+
     authentification(login: any): Observable<any> {
         console.log("Authentification...")
         return this.http.post(this.uri + '/auth/login', login);
@@ -24,7 +24,10 @@ export class AuthService {
     getUser(id: number): Observable<Login> {
         return this.http.get<Login>(this.uri + "/user/" + id)
     }
-    updateUser(login:Login):Observable<any> {
-        return this.http.put(this.uri+ "/user/", login);
-      }
+    updateUser(login: Login): Observable<any> {
+        return this.http.put(this.uri + "/user/", login);
+    }
+    demandeJeton(jeton: any): Observable<any> {
+        return this.http.post(this.uri + '/jeton/demande', jeton);
+    }
 }
